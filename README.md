aebd2c2c73df67b90b024b0c66ddfb7d9fa8e47b
Georgio Diab <georgiod9@gmail.com>
2022-12-06 19:29:51 +0400

    Dynamically create pools

---
+++ README.md
@@ -0,0 +1,9 @@
+# DEX Token Scanner
+
+## First time use
+Run 
+`npm install`
+
+## How to run
+
+`npx ts-node index.ts`
--- index.ts
+++ index.ts
@@ -11,10 +11,17 @@ import { abi as IUniswapV3PoolABI } from '@uniswap/v3-core/artifacts/contracts/i
 
 
 interface PoolObject {
-  pool_id: String
+  pool_id: string
   token0: Token
   token1: Token
 } 
+
+interface PriceModel {
+  token0: string | undefined,
+  token1: string | undefined,
+  price: string,
+  
+}
 
 let chain = new Chain();
 let init = new Chain();
@@ -27,6 +34,7 @@ let pools_list: Array<PoolObject>= [];
 ////  console.log("Pool list variable: ", pools_list[0])
 //}
 
+//getAllUniswapPools()
 init.add(
   () => {
     getAllUniswapPools()
@@ -41,49 +49,62 @@ init.add(
 async function  getAllUniswapPools(){
   const query = `
     {
-      pools(first: 10) {
+      pools(first: 10, orderBy: volumeUSD, orderDirection: desc) {
         id
         token0{
           id
           name
           symbol
+          decimals
         }
         token1 {
           id
           name
           symbol
+          decimals
         }
       }
       
     }
   `
-  const res = fetch('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: JSON.stringify({
-      query
+  try {
+    const res = await fetch('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        query
+      })
     })
-  })
-  .then(r => r.json())
-  .then(data => {
-    //console.log('data returned: ', data.data.pools[0]);
-    //setPoolsList(data.data.pools);
-
-    const pools = data.data.pools;
-    //console.log("DATA RET: ", pools)
-    pools.forEach((pool: any) => {
-      const model: PoolObject = {
-        pool_id: pool.id,
-        token0: pool.token0,
-        token1: pool.token1
+    .then(r => r.json())
+    .then(data => {
+      //console.log('data returned: ', data.data.pools[0]);
+      //setPoolsList(data.data.pools);
+  
+      if (data.data.pools) {
+        const pools = data.data.pools;
+        //console.log("DATA RET: ", pools)
+        pools.forEach((pool: any) => {
+        //console.log("DATA RET: ", pool.token0.id)
+  
+          const model: PoolObject = {
+            pool_id: pool.id,
+            token0: new Token(3, pool.token0.id, parseInt(pool.token0.decimals?.toString()), pool.token0.symbol?.toString(), pool.token0.name?.toString()),
+            token1: new Token(3, pool.token1.id, parseInt(pool.token1.decimals?.toString()), pool.token1.symbol?.toString(), pool.token1.name?.toString())
+          }
+          pools_list.push(model)
+        });
       }
-      pools_list.push(model)
+      
+      console.log("ARRAY FINAL: ", pools_list)
     });
-    //console.log("ARRAY FINAL: ", pools_list)
-  });
+  }
+  catch (e: any) {
+    console.log(e)
+  }
+ 
 
   
 }
@@ -152,16 +173,23 @@ async function getPoolState() {
   return PoolState
 }
 
-async function readPoolInstance(): Promise<Pool> {
+async function readPoolInstance(
+  poolObj: PoolObject
+
+): Promise<Pool> {
   const [immutables, state] = await Promise.all([getPoolImmutables(), getPoolState()])
+  //console.log("Pool Obj TOken0: ",  poolObj.token0)
 
-  const TokenA = new Token(3, immutables.token0, 6, 'USDC', 'USD Coin')
+  //console.log("Pool Obj TOken1: ", poolObj.token1)
 
-  const TokenB = new Token(3, immutables.token1, 18, 'WETH', 'Wrapped Ether')
+
+  const TokenA = poolObj.token0;
+
+  const TokenB = poolObj.token1;
 
   const pool = new Pool(
-    TokenA,
     TokenB,
+    TokenA,
     immutables.fee,
     state.sqrtPriceX96.toString(),
     state.liquidity.toString(),
@@ -172,46 +200,70 @@ async function readPoolInstance(): Promise<Pool> {
   return pool;
 }
 
+function main(){
+  console.log("STARTING")
+  if(pools_list) {
+    //console.log("Uniswap pools: ", pools_list[0])
+    //console.log("Fetched Uniswap Pools :", pools_list)
+    const l: Array<PriceModel> = []
+    pools_list.forEach(pool => {
+      poolContract = new ethers.Contract(pool.pool_id, IUniswapV3PoolABI, provider);
+      //console.log("current pool instance: ", pool)
+      //console.log("TOKEN0-: ", pool.token0.decimals, pool.token0)
+      //console.log("TOKEN1-: ",  pool.token1.decimals,  pool.token1)
+      
+
+      try {
+        const poolInstance = readPoolInstance(
+          pool
+        );
+        poolInstance.then((poolData) => {
+
+          ////console.log("***POOL***")
+          ////console.log("POOL ID: ", pool.pool_id)
+          //console.log(p)
+          ////console.log("->Pool Data")
+          //console.log("Uniswap Pools list: ", pools_list)
+          const token0Price = poolData.token0Price;
+          const token1Price = poolData.token1Price;
+          //console.log("Base Currency: ", token0Price)
+        
+          const sqrtx96 = poolData.sqrtRatioX96;
+          const priceToken0 = token0Price.toSignificant(6)
+          //console.log("Price of token0 wrt token1: ", priceToken0)
+
+          const priceToken1 = token1Price.toSignificant(6)
+          console.log(`Price of ${poolData.token1.symbol} wrt ${poolData.token0.symbol}: ${priceToken1}`)
+          
+          let model: PriceModel = {
+            token0: poolData.token1.symbol,
+            token1: poolData.token0.symbol,
+            price: priceToken1
+          } 
+
+          l.push(model)
+        })
+        .then((data) => console.log("CREATD LIST: ", l))
+      }
+      catch (e: any){
+        console.log(e)
+      }
+      
+    });
+
+  }
+}
+
+//main()
+
 chain
   .add(
     () => {
-      if(pools_list) {
-        //console.log("Uniswap pools: ", pools_list[0])
-        //console.log("Fetched Uniswap Pools :", pools_list)
-
-        pools_list.forEach(pool => {
-          //poolContract = new ethers.Contract(pool.id, IUniswapV3PoolABI, provider);
-          //console.log("current pool instance: ", pool)
-          const poolInstance = readPoolInstance(
-            
-          );
-          poolInstance.then((poolData) => {
-
-            console.log("***POOL***")
-            //console.log(p)
-            console.log("->Pool Data")
-            console.log("Uniswap Pools list: ", pools_list)
-            const token0Price = poolData.token0Price;
-            const token1Price = poolData.token1Price;
-            //console.log("Base Currency: ", token0Price)
-          
-            const sqrtx96 = poolData.sqrtRatioX96;
-            const priceToken0 = token0Price.toSignificant(6)
-            //console.log("Price of token0 wrt token1: ", priceToken0)
-  
-            const priceToken1 = token1Price.toSignificant(6)
-            console.log(`Price of ${poolData.token1.symbol} wrt ${poolData.token0.symbol}: ${priceToken1}`)
-  
-          })
-        });
-
-        
-        
-      }
-
+      main()
     }
+    
   )
-  .every(2000)
+  .every(20000)
   .forever
 
 
